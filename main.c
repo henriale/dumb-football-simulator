d57e8c1f6363ba85e7fd34eedbbacb39322a3578
Alexandre Araujo <alexandreh.araujo@gmail.com>
2017-06-23 15:52:32 -0300

    adiciona demais jogadores

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <chipmunk.h>
 #include "opengl.h"
 
-/**
- * Funções para movimentação de objetos
- * @todo: extract
- */
-void moveRobo(cpBody *body, void *data);
+typedef unsigned int fieldZone;
+
+fieldZone bodyZone(cpBody *body);
+
+void moveAttackerToTheBall(cpBody *playerBody, void *data);
+void moveAttackerToOrigin(cpBody *body, void *data);
 void moveDefenderToTheBall(cpBody *body, void *data);
 void moveDefenderToOrigin(cpBody *body, void *data);
 void moveGoalkepper(cpBody *body, void *data);
 void waitForBallToApproach(cpBody *body, void *data);
 
 bool ballIsInDefendingZone(cpBody *playerBody, cpBody *ballBody);
+bool ballIsInMidfield(cpBody *playerBody, cpBody *ballBody);
 bool playerIsInAttackingZone(cpBody *playerBody);
 bool playerIsInMidfield(cpBody *playerBody);
 bool playerIsInDefendingZone(cpBody *playerBody);
@@ -33,8 +35,7 @@ const static int SLOW_PACE = 15;
 const static int NORMAL_PACE = 40;
 const static int FAST_PACE = 85;
 
-typedef unsigned int fieldZone;
-fieldZone bodyZone(cpBody *body);
+const static double ELAST_DEFAULT = 1.3;
 
 /**
  * Score do jogo
@@ -102,50 +103,19 @@ void initCM()
   // todo: extract
   ballBody = newCircle(cpv(512, 350), 8, 1, "../images/ball.png", NULL, 0.2, 1);
 
-  playersBody[0] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.2, 350+rand()%100-50), 20, 5, moveGoalkepper, 0.2, 0.9);
-  playersBody[1] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.5+rand()%30-15, 150+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, 0.9);
-  playersBody[2] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.5+rand()%30-15, 350+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, 0.9);
-  playersBody[3] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.5+rand()%30-15, 550+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, 0.9);
-  playersBody[4] = newPlayer(TEAM_A, cpv(350, 350), 20, 5, moveRobo, 0.2, 0.9);
+  playersBody[0] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.2, 350+rand()%100-50), 20, 5, moveGoalkepper, 0.2, ELAST_DEFAULT);
+  playersBody[1] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.5+rand()%30-15, 150+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, ELAST_DEFAULT);
+  playersBody[2] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.5+rand()%30-15, 350+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, ELAST_DEFAULT);
+  playersBody[3] = newPlayer(TEAM_A, cpv(ZONE_SIZE*0.5+rand()%30-15, 550+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, ELAST_DEFAULT);
+  playersBody[4] = newPlayer(TEAM_A, cpv(350, 250), 20, 5, moveAttackerToTheBall, 0.2, ELAST_DEFAULT);
+  playersBody[5] = newPlayer(TEAM_A, cpv(350, 450), 20, 5, moveAttackerToTheBall, 0.2, ELAST_DEFAULT);
 
-  playersBody[6] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.5+rand()%30-15, 150+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, 0.9);
-  playersBody[7] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.5+rand()%30-15, 350+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, 0.9);
-  playersBody[8] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.5+rand()%30-15, 550+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, 0.9);
-}
-
-/**
- * Exemplo de função de movimentação: move o robô em direção à bola
- *
- * @param body
- * @param data
- *
- * @todo: extract
- */
-void moveRobo(cpBody *body, void *data)
-{
-  // Veja como obter e limitar a velocidade do robô...
-  cpVect vel = cpBodyGetVelocity(body);
-  // printf("vel: %f %f", vel.x,vel.y);
-
-  // Limita o vetor em 50 unidades
-  vel = cpvclamp(vel, 90);
-  // E seta novamente a velocidade do corpo
-  cpBodySetVelocity(body, vel);
-
-  // Obtém a posição do robô e da bola...
-  cpVect robotPos = cpBodyGetPosition(body);
-  cpVect ballPos = cpBodyGetPosition(ballBody);
-
-  // Calcula um vetor do robô à bola (DELTA = B - R)
-  cpVect pos = robotPos;
-  pos.x = -robotPos.x;
-  pos.y = -robotPos.y;
-  cpVect delta = cpvadd(ballPos, pos);
-
-  // Limita o impulso em 20 unidades
-  delta = cpvmult(cpvnormalize(delta), 20);
-  // Finalmente, aplica impulso no robô
-  cpBodyApplyImpulseAtWorldPoint(body, delta, robotPos);
+  playersBody[6] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.8, 350+rand()%100-50), 20, 5, moveGoalkepper, 0.2, ELAST_DEFAULT);
+  playersBody[7] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.5+rand()%30-15, 150+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, ELAST_DEFAULT);
+  playersBody[8] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.5+rand()%30-15, 350+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, ELAST_DEFAULT);
+  playersBody[9] = newPlayer(TEAM_B, cpv(ZONE_SIZE*2+ZONE_SIZE*0.5+rand()%30-15, 550+rand()%30-15), 20, 5, waitForBallToApproach, 0.2, ELAST_DEFAULT);
+  playersBody[10] = newPlayer(TEAM_B, cpv(600, 250), 20, 5, moveAttackerToTheBall, 0.2, ELAST_DEFAULT);
+  playersBody[11] = newPlayer(TEAM_B, cpv(600, 450), 20, 5, moveAttackerToTheBall, 0.2, ELAST_DEFAULT);
 }
 
 /**
@@ -203,6 +173,30 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+/**
+ *
+ * @param body
+ *
+ * @return
+ */
+fieldZone bodyZone(cpBody *body)
+{
+  cpVect position = cpBodyGetPosition(body);
+
+  if (position.x < ZONE_SIZE) {
+      return LEFT_ZONE;
+    }
+
+  if (position.x >= ZONE_SIZE && position.x <= ZONE_SIZE*2) {
+      return MID_ZONE;
+    }
+
+  if (position.x > ZONE_SIZE*2) {
+      return RIGHT_ZONE;
+    }
+}
+
 
 /**
  * Cria e adiciona uma nova linha estática (segmento) ao ambiente
@@ -271,6 +265,72 @@ cpBody *newCircle(cpVect pos, cpFloat radius, cpFloat mass, char *img, bodyMotio
 }
 
 /**
+ * Create new circle body and set a team to its user data
+ *
+ * @param team
+ * @param pos
+ * @param radius
+ * @param mass
+ * @param img
+ * @param motion
+ * @param fric
+ * @param elast
+ *
+ * @return cpBody
+ */
+cpBody * newPlayer(playerTeam team, cpVect pos, cpFloat radius, cpFloat mass, bodyMotionFunc motion, cpFloat fric, cpFloat elast)
+{
+  char *img;
+  if (team == TEAM_A) {
+      img = "../images/player1.png";
+    } else {
+      img = "../images/player2.png";
+    }
+
+  cpBody *body = newCircle(pos, radius, mass, img, motion, fric, elast);
+
+  UserData *userData = cpBodyGetUserData(body);
+  userData->team = team;
+  userData->defaultPosition = pos;
+
+  return body;
+}
+
+/**
+ * Exemplo de função de movimentação: move o robô em direção à bola
+ *
+ * @param playerBody
+ * @param data
+ *
+ * @todo: extract
+ */
+void moveAttackerToTheBall(cpBody *playerBody, void *data)
+{
+  cpVect vel = cpBodyGetVelocity(playerBody);
+  vel = cpvclamp(vel, NORMAL_PACE);
+  cpBodySetVelocity(playerBody, vel);
+
+  cpVect robotPos = cpBodyGetPosition(playerBody);
+  cpVect ballPos = cpBodyGetPosition(ballBody);
+
+  UserData *userData = data;
+
+  if (! playerIsInMidfield(playerBody)) {
+      userData->motionFunction = moveAttackerToOrigin;
+  }
+  // Calcula um vetor do robô à bola (DELTA = B - R)
+  cpVect pos = robotPos;
+  pos.x = -robotPos.x;
+  pos.y = -robotPos.y;
+  cpVect delta = cpvadd(ballPos, pos);
+
+  // Limita o impulso em 20 unidades
+  delta = cpvmult(cpvnormalize(delta), 20);
+  // Finalmente, aplica impulso no robô
+  cpBodyApplyImpulseAtWorldPoint(playerBody, delta, robotPos);
+}
+
+/**
  *
  * @param body
  * @param data
@@ -331,6 +391,7 @@ void moveGoalkepper(cpBody *body, void *data)
 
   cpBodyApplyImpulseAtWorldPoint(body, delta, playerPosition);
 }
+
 /**
  *
  * @param body
@@ -346,6 +407,34 @@ void moveDefenderToOrigin(cpBody *body, void *data)
   if (ballIsInDefendingZone(body, ballBody)) {
       printf("changing defender motion to the ball\n");
       userData->motionFunction = moveDefenderToTheBall;
+      return;
+  }
+
+  cpVect playerPosition = cpBodyGetPosition(body);
+  cpVect pos = playerPosition;
+  pos.x = -playerPosition.x;
+  pos.y = -playerPosition.y;
+  cpVect delta = cpvadd(userData->defaultPosition, pos);
+  delta = cpvmult(cpvnormalize(delta), 20);
+
+  cpBodyApplyImpulseAtWorldPoint(body, delta, playerPosition);
+}
+
+/**
+ *
+ * @param body
+ * @param data
+ */
+void moveAttackerToOrigin(cpBody *body, void *data)
+{
+  cpVect vel = cpBodyGetVelocity(body);
+  vel = cpvclamp(vel, SLOW_PACE);
+  cpBodySetVelocity(body, vel);
+  UserData *userData = data;
+
+  if (! ballIsInMidfield(body, ballBody)) {
+      printf("changing attacker motion to the ball\n");
+      userData->motionFunction = moveAttackerToTheBall;
       return;
   }
 
@@ -392,6 +481,28 @@ bool ballIsInDefendingZone(cpBody *playerBody, cpBody *ballBody)
   fieldZone zone = bodyZone(ballBody);
 
   if ((team == TEAM_A && zone == LEFT_ZONE) || (team == TEAM_B && zone == RIGHT_ZONE)) {
+      return true;
+  }
+
+  return false;
+}
+
+/**
+ *
+ * @param playerBody
+ * @param ballBody
+ *
+ * @return
+ */
+bool ballIsInMidfield(cpBody *playerBody, cpBody *ballBody)
+{
+  UserData *userData = cpBodyGetUserData(playerBody);
+  playerTeam team = userData->team;
+
+  fieldZone zone = bodyZone(ballBody);
+
+  // TODO: refactor
+  if ((team == TEAM_A && zone == MID_ZONE) || (team == TEAM_B && zone == MID_ZONE)) {
       return true;
   }
 
@@ -451,58 +562,4 @@ bool playerIsInMidfield(cpBody *playerBody)
   }
 
   return false;
-}
-
-/**
- * Create new circle body and set a team to its user data
- *
- * @param team
- * @param pos
- * @param radius
- * @param mass
- * @param img
- * @param motion
- * @param fric
- * @param elast
- *
- * @return cpBody
- */
-cpBody * newPlayer(playerTeam team, cpVect pos, cpFloat radius, cpFloat mass, bodyMotionFunc motion, cpFloat fric, cpFloat elast)
-{
-  char *img;
-  if (team == TEAM_A) {
-    img = "../images/player1.png";
-  } else {
-      img = "../images/player2.png";
-  }
-
-  cpBody *body = newCircle(pos, radius, mass, img, motion, fric, elast);
-
-  UserData *userData = cpBodyGetUserData(body);
-  userData->team = team;
-  userData->defaultPosition = pos;
-
-  return body;
-}
-
-/**
- *
- * @param body
- * @return
- */
-fieldZone bodyZone(cpBody *body)
-{
-  cpVect position = cpBodyGetPosition(body);
-
-  if (position.x < ZONE_SIZE) {
-      return LEFT_ZONE;
-    }
-
-  if (position.x >= ZONE_SIZE && position.x <= ZONE_SIZE*2) {
-      return MID_ZONE;
-    }
-
-  if (position.x > ZONE_SIZE*2) {
-      return RIGHT_ZONE;
-    }
 }
